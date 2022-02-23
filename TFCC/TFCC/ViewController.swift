c1ce28d17853e087f943f3d31d69811e256b95e5
Rizwan Walayat <rizwan.wallayat@gmail.com>
2022-02-23 14:30:06 +0500

    New Project Setup

---
+++ TFCC/TFCC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TFCC
+//
+//  Created by Phaedra Solutions  on 23/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
