97eb035cba0a6ccacd46c3f548ed696460373c9a
Rizwan Walayat <rizwan.wallayat@gmail.com>
2022-03-04 19:10:53 +0500

    Physician Listing Table

---
+++ TFCC/Controllers/PhysicianListing/Cells/PhysicianListingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhysicianListingTableViewCell.swift
+//  TFCC
+//
+//  Created by Phaedra Solutions  on 04/03/2022.
+//
+
+import UIKit
+
+class PhysicianListingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TFCC/Controllers/PhysicianListing/PhysicianListingViewController.swift
+++ TFCC/Controllers/PhysicianListing/PhysicianListingViewController.swift
@@ -10,10 +10,9 @@ import UIKit
 class PhysicianListingViewController: UIViewController {
 
     //MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchTextField: UITextField!
-    @IBOutlet weak var bannersCollectionView: UICollectionView!
-    @IBOutlet weak var categoriesCollectionView: UICollectionView!
-    @IBOutlet weak var categoriesCollectionViewHeightConstraint: NSLayoutConstraint!
+
     
     //MARK: - Variables
     var coordinator: MainCoordinator?
@@ -28,8 +27,7 @@ class PhysicianListingViewController: UIViewController {
     //MARK: - Private Methods
     private func setup() {
         searchTextField.delegate = self
-        bannersCollectionView.register(UINib(nibName: "BannerCollectionCell", bundle: nil), forCellWithReuseIdentifier: "bannerCollectionCell")
-        categoriesCollectionView.register(UINib(nibName: "CategoriesCollectionCell", bundle: nil), forCellWithReuseIdentifier: "categoriesCollectionCell")
+      
     }
     
     //MARK: - IBActions
@@ -49,46 +47,3 @@ extension PhysicianListingViewController: UITextFieldDelegate {
     }
 }
 
-//MARK: - UICollectionView Delegates
-extension PhysicianListingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == bannersCollectionView {
-            return 2
-        }
-        else if collectionView == categoriesCollectionView {
-            let heightOfcollectionViewLineSpacing = (11 / 3) * 30
-            let heightOfCollectionViewCells = ((11 / 3) * (bannersCollectionView.bounds.width/3 - (32 - 29)))
-            categoriesCollectionViewHeightConstraint.constant = CGFloat(heightOfcollectionViewLineSpacing) + heightOfCollectionViewCells + 50
-            return 11
-        }
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        if collectionView == bannersCollectionView {
-            let cell = bannersCollectionView.dequeueReusableCell(withReuseIdentifier: "bannerCollectionCell", for: indexPath) as! BannerCollectionCell
-            
-            return cell
-        }
-        else if collectionView == categoriesCollectionView {
-            
-            let cell = categoriesCollectionView.dequeueReusableCell(withReuseIdentifier: "categoriesCollectionCell", for: indexPath) as! CategoriesCollectionCell
-            cell.circularView.cornerRadius = (bannersCollectionView.bounds.width/3 - 32) / 2
-            cell.config(indexPath: indexPath)
-            
-            return cell
-        }
-        return UICollectionViewCell()
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        if collectionView == bannersCollectionView {
-            return CGSize(width: bannersCollectionView.bounds.width - 32, height: (bannersCollectionView.bounds.width-32)*170/325)
-        }
-        else if collectionView == categoriesCollectionView {
-            return CGSize(width: bannersCollectionView.bounds.width/3 - 32, height: bannersCollectionView.bounds.width/3 - (32 - 29))
-        }
-        return CGSize(width: 0, height: 0)
-    }
-}
